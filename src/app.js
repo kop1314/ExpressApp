220c3d6ce00eb77c5896cc199c74d3ffe62f9274
Zhouzhou Yang <zy15@illinois.edu>
2022-05-21 22:56:18 -0400

    default route

--- src/app.js
+++ src/app.js
@@ -7,12 +7,14 @@ const app = express();
 //app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/', (req, res) => {
+
+app.use('/', authRouter);
+app.use('/', postsRouter);
+
+app.get('/', (req, res) => {
     return res.status(200).json({
         message: "hello world",
     });
 });
-app.use('/', authRouter);
-app.use('/', postsRouter);
 
 module.exports = app;
